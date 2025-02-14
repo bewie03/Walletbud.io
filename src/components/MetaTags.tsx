feb77aa248a40ad7f95fb0f6beb2da4c4019e31c
bewie03 <bewie03@gmail.com>
2025-02-14 17:24:45 +1100

    fix: reorder meta tags to display image underneath in Discord embed

--- src/components/MetaTags.tsx
+++ src/components/MetaTags.tsx
@@ -12,14 +12,14 @@ export const MetaTags = ({
   return (
     <Helmet>
       <title>{title}</title>
+      <meta name="theme-color" content="#3498db" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="WALLET PUP" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="https://walletpup.com/Banner.png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
-      <meta name="theme-color" content="#3498db" />
+      <meta property="og:image" content="https://walletpup.com/Banner.png" />
     </Helmet>
   );
 };
